fa9bb269738624d19cda4f09ae86a7f187dff8e7
Fesor <onafesowale@gmail.com>
2022-12-04 05:19:17 -0800

    Migrations config with IEntityTypeConfiguration

---
+++ Infrastructure/Data/Configuration/GamesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configuration
+{
+    internal class GamesConfiguration : IEntityTypeConfiguration<Games>
+    {
+        public void Configure(EntityTypeBuilder<Games> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(100);
+            builder.Property(x => x.Description).HasMaxLength(500);
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.HasOne(x => x.Genre).WithMany().HasForeignKey(c => c.GenreId);
+            builder.HasOne(x => x.ConsoleDevice).WithMany().HasForeignKey(p => p.ConsoleDeviceId);
+        }
+    }
+}
