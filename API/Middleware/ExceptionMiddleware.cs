d13121d7c4aeb84977b93e39e7be95173dd7b1be
Fesor <onafesowale@gmail.com>
2022-12-06 19:35:21 -0800

    Created our middleware to catch errors and return it as a type of  ApiResponse or ApiException object depending on the env

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using API.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
+
+        //we brought in the IHostEnvironment bcos we want to return a certain error if we are in development and a different type if we are not
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            
+            this.next = next;
+            this.logger = logger;
+            this.env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                //if there is no exception, we want the middleware to move on to the next
+                //so if there is no exception, the request moves on to the next stage
+                await next(context);
+            }
+
+            catch (Exception ex)
+            {
+                //if there is an exception, we log it with the message
+                logger.LogError(ex, ex.Message);
+
+                //we will also write our response to the context so we can send it to the client
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = env.IsDevelopment() ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+                    : new ApiException((int)HttpStatusCode.InternalServerError);
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);    
+            }
+        }
+
+    }
+}
