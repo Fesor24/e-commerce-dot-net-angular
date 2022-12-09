5c6637a98701eb01508eb4bbe413470599208854
Fesor <onafesowale@gmail.com>
2022-12-08 20:58:18 -0800

    Added Props for orderByExpression, orderByDescExpression and Paging properties

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -11,5 +11,11 @@ namespace Core.Specifications
     {
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderByExpression { get; }
+        Expression<Func<T, object>> OrderByDescendingExpression { get; }
+        int Take { get; }
+        int Skip { get; }
+        bool IsPagingEnabled { get; }
+
     }
 }
