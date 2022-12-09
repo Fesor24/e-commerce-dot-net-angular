18f926d9199aa15656599f337e6e00ec9a41890f
Fesor <onafesowale@gmail.com>
2022-12-08 20:53:26 -0800

    Specification class for counting the data whether or not filters have been applied

---
+++ Core/Specifications/GamesWithFiltersCountSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class GamesWithFiltersCountSpecification : BaseSpecification<Games>
+    {
+        public GamesWithFiltersCountSpecification(GamesSpecParams specParams)
+            :base(x => 
+            (!specParams.GenreId.HasValue || x.GenreId == specParams.GenreId) &&
+            (!specParams.DeviceId.HasValue || x.ConsoleDeviceId == specParams.DeviceId) &&
+            (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search))
+            )
+        {
+
+        }
+    }
+}
