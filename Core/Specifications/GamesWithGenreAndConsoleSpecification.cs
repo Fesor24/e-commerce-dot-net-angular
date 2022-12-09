8e4fda6b367dbc3a2628a81a1b41445489b3756b
Fesor <onafesowale@gmail.com>
2022-12-08 21:00:00 -0800

    Implemented filter, paging and orderBy functionalities

--- Core/Specifications/GamesWithGenreAndConsoleSpecification.cs
+++ Core/Specifications/GamesWithGenreAndConsoleSpecification.cs
@@ -9,10 +9,38 @@ namespace Core.Specifications
 {
     public class GamesWithGenreAndConsoleSpecification: BaseSpecification<Games>
     {
-        public GamesWithGenreAndConsoleSpecification()
+        public GamesWithGenreAndConsoleSpecification(GamesSpecParams specParams): base(
+            x =>
+            (!specParams.DeviceId.HasValue || x.ConsoleDeviceId == specParams.DeviceId) &&
+            (!specParams.GenreId.HasValue || x.GenreId == specParams.GenreId) &&
+            (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search))
+            )
         {
             AddIncludes(x => x.Genre);
             AddIncludes(x => x.ConsoleDevice);
+            ApplyPaging(specParams.PageSize, (specParams.PageIndex - 1) * specParams.PageSize);
+
+            if (!string.IsNullOrEmpty(specParams.Sort))
+            {
+                switch (specParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(x => x.Price);
+                        break;
+
+                    case "priceDesc":
+                        AddOrderByDescending(x => x.Price);
+                        break;
+
+                    case "nameDesc":
+                        AddOrderByDescending(x => x.Name);
+                        break;
+
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
         public GamesWithGenreAndConsoleSpecification(int id): base(x => x.Id == id)
         {
