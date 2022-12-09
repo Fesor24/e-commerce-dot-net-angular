72f5b53458e23f3f0d313199ead12cef63d5ec31
Fesor <onafesowale@gmail.com>
2022-12-08 20:57:30 -0800

    Added evaluations for orderBy, orderByDesc and Paging

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -17,6 +17,14 @@ namespace Infrastructure.Data
             
             if(spec.Criteria is not null) query = query.Where(spec.Criteria);
 
+            if (spec.OrderByExpression is not null) query = query.OrderBy(spec.OrderByExpression);
+
+            if(spec.OrderByDescendingExpression is not null) query = query.OrderByDescending(spec.OrderByDescendingExpression);
+
+
+            //we set isPagingEnabled to true in our BaseSpec so paging will be enabled as the condition is true
+            if (spec.IsPagingEnabled) query = query.Skip(spec.Skip).Take(spec.Take);
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
