54c5c8a3fc2b007ac2fc4aef00a17c324ff59972
Fesor <onafesowale@gmail.com>
2022-12-12 19:35:45 -0800

    Added shoppingCartItem class to project

---
+++ Core/Entities/ShoppingCartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; }
+        public string Genre { get; set; }
+        public string Device { get; set; }
+    }
+}
