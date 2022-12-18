dce65ffdac09ba27bae9f9244c8d64c9606dbcb6
Fesor <onafesowale@gmail.com>
2022-12-18 03:32:24 -0800

    Created DTOs so we could add validations from data we are getting from client

---
+++ API/DTOs/ShoppingCartDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Entities;
+
+namespace API.DTOs
+{
+    public class ShoppingCartDto
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public List<ShoppingCartItemDto> Items { get; set; } = new List<ShoppingCartItemDto>();
+    }
+}
