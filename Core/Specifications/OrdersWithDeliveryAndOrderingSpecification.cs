554e10c9cf0bae660b3fcf260ebe73b8c4867354
Fesor <onafesowale@gmail.com>
2022-12-22 18:35:21 -0800

    OrdersWithDeliverySpec

---
+++ Core/Specifications/OrdersWithDeliveryAndOrderingSpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications
+{
+    public class OrdersWithDeliveryAndOrderingSpecification : BaseSpecification<Order>
+    {
+        public OrdersWithDeliveryAndOrderingSpecification(string email) : base(x => x.BuyerEmail == email)
+        {
+            AddIncludes(x => x.OrderItems);
+            AddIncludes(x => x.DeliveryMethods);
+            AddOrderByDescending(x => x.OrderDate);
+        }
+
+        public OrdersWithDeliveryAndOrderingSpecification(int orderId, string email): base(x => x.Id == orderId && x.BuyerEmail == email)
+        {
+            AddIncludes(x => x.OrderItems);
+            AddIncludes(x => x.DeliveryMethods);
+        }
+    }
+}
