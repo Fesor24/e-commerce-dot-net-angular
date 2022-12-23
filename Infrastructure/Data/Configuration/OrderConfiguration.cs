f4162fefc73eacf4a9cd625cd5a2a217f6e1951f
Fesor <onafesowale@gmail.com>
2022-12-22 18:32:10 -0800

    EntityTypeConfiguration for our order

---
+++ Infrastructure/Data/Configuration/OrderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configuration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(x => x.ShipToAddress, b =>
+            {
+                b.WithOwner();
+            });
+
+            builder.Property(s => s.Status)
+                .HasConversion(p => p.ToString(),
+                p => (OrderStatus) Enum.Parse(typeof(OrderStatus), p));
+
+            builder.HasMany(o => o.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(x => x.SubTotal).HasColumnType("decimal(18,2)");
+        }
+    }
+}
