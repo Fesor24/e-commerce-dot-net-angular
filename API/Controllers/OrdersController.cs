fcacd74550d47157cf6569b5bed04ff2df98180d
Fesor <onafesowale@gmail.com>
2022-12-22 18:28:17 -0800

    OrderController

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Claims;
+using API.DTOs;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService orderService;
+        private readonly IMapper mapper;
+
+        public OrdersController(IOrderService orderService, IMapper mapper)
+        {
+            this.orderService = orderService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
+        {
+            var email = User.RetrieveEmailFromClaimsPrinciple();
+
+            var address = mapper.Map<AddressDto, Address>(orderDto.ShipToAddress);
+
+            var order = await orderService.CreateOrderAsync(email,orderDto.DeliveryMethodId, orderDto.CartId, address);
+
+            if (order is null) return BadRequest(new ApiResponse(400, "Problem creating order"));
+
+            return Ok(order);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            var email = User.RetrieveEmailFromClaimsPrinciple();
+
+            var orders = await orderService.GetOrderForUserAsync(email);
+
+            return Ok(mapper.Map<IReadOnlyList<Order>, IReadOnlyList<OrderToReturnDto>>(orders));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id)
+        {
+            var email = User.RetrieveEmailFromClaimsPrinciple();
+
+            var order = await orderService.GetOrderByIdAsync(id, email);
+
+            if (order is null) return NotFound(new ApiResponse(404));
+
+            return Ok(mapper.Map<Order, OrderToReturnDto>(order));
+        }
+
+        [HttpGet("delivery-methods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            return Ok(await orderService.GetDeliveryMethodsAsync());
+        }
+    }
+}
