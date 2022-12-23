12d98152a38407a1b167946a2fe87e4cc2c902aa
Fesor <onafesowale@gmail.com>
2022-12-22 18:37:18 -0800

    Added tables to our db

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities.OrderAggregate;
 
 namespace Infrastructure.Data
 {
@@ -25,5 +26,8 @@ namespace Infrastructure.Data
         public DbSet<Games> Games { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<ConsoleDevice> ConsoleDevices { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set;}
     }
 }
