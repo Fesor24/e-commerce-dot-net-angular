c682a9827bdde27ca7f6b786fdea42c85103bd98
Fesor <onafesowale@gmail.com>
2022-12-22 18:33:21 -0800

    Handle generating jwt's

---
+++ Infrastructure/Data/Services/TokenService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Data.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration config;
+
+        private readonly SymmetricSecurityKey key;
+
+        public TokenService(IConfiguration config)
+        {
+            this.config = config;
+            this.key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.config["Token:Key"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.DisplayName)
+            };
+
+            //Credentials
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //What do we want inside our token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(3),
+                SigningCredentials = creds,
+                Issuer = config["Token:Issuer"]
+            };
+
+            //We need something that can handle the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
