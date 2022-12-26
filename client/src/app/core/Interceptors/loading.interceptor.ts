7bee0199bfb7ef7df148a2fb66652805121cb0c5
Fesor <onafesowale@gmail.com>
2022-12-26 00:03:05 -0800

    Loading interceptor

--- client/src/app/core/Interceptors/loading.interceptor.ts
+++ client/src/app/core/Interceptors/loading.interceptor.ts
@@ -14,6 +14,12 @@ export class LoadingInterceptor implements HttpInterceptor {
   constructor(private busyService: BusyService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if(request.method === 'POST' && request.url.includes('orders')){
+      return next.handle(request);
+    }
+
+    this.busyService.busy();
+
     return next.handle(request).pipe(
       delay(1000),
       finalize(() => {
