7d9b642d5fa5a67c6250a56c2ab7e0eb8a36ee7b
Fesor <onafesowale@gmail.com>
2022-12-26 01:33:18 -0800

    Entities to take care of our wishlist

---
+++ Core/Entities/Wishlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Wishlist
+    {
+        public Wishlist()
+        {
+
+        }
+
+        public Wishlist(string id)
+        {
+            Id = id;
+        }
+        public string Id { get; set; }
+
+        public List<WishlistItem> Items { get; set; } = new List<WishlistItem>();
+    }
+}
---
+++ Core/Entities/WishlistItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class WishlistItem
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Genre { get; set; }
+        public string Device { get; set; }
+    }
+}
