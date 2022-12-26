e8b22b7f94237cf6f44ec25e8c6d8998b3c59d7e
Fesor <onafesowale@gmail.com>
2022-12-25 23:49:54 -0800

    Added additional properties to our shopping cart dto to aid payment processing

--- API/DTOs/ShoppingCartDto.cs
+++ API/DTOs/ShoppingCartDto.cs
@@ -9,5 +9,13 @@ namespace API.DTOs
         public string Id { get; set; }
         [Required]
         public List<ShoppingCartItemDto> Items { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+
+        public string? ClientSecret { get; set; }
+
+        public string? PaymentIntentId { get; set; }
+
+        public decimal ShippingPrice { get; set; }
     }
 }
