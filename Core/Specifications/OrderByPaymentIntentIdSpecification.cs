39cceaf1b1949a09ee1e3c00bc2eaae5492adb98
Fesor <onafesowale@gmail.com>
2022-12-25 23:54:04 -0800

    Order by PaymentIntentId specification class

---
+++ Core/Specifications/OrderByPaymentIntentIdSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Core.Specifications
+{
+    public class OrderByPaymentIntentIdSpecification: BaseSpecification<Order>
+    {
+        public OrderByPaymentIntentIdSpecification(string paymentIntentId): base(x => x.PaymentIntentId == paymentIntentId)
+        {
+
+        }
+    }
+}
