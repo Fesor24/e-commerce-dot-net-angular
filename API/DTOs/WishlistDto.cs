13b0cec37e8b91b63be2e8658255411e77ce6e97
Fesor <onafesowale@gmail.com>
2022-12-26 01:31:51 -0800

    DTO's for our wishlist

---
+++ API/DTOs/WishlistDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class WishlistDto
+    {
+        [Required]
+        public string Id { get; set; }
+        public List<WishlistItemDto> Items { get; set; }
+    }
+}
---
+++ API/DTOs/WishlistItemDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class WishlistItemDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string GameName { get; set; }
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+      
+        [Required]
+        public string PictureUrl { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Device { get; set; }
+    }
+}
